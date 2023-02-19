24291405fae18d95c32573228a0cae3005d41483
Adrienn Agij <108215852+odrienn@users.noreply.github.com>
2023-02-19 15:32:15 +0100

    Codewars 6 kyu: Find the odd int

---
+++ prepare_for_the_exam/find_odd_int.js
@@ -0,0 +1,25 @@
+function findOdd(A) {
+  let result = 0;
+  let count = 0;
+  for (let i = 0; i < A.length; i++) {
+    for (let j = 0; j < i ; j++){
+      if (A[j] > A[i]){
+        let temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+      }
+    }
+  }
+  for (let i = 0; i < A.length; i++){
+    if( A[i] === A[i+1]){
+      count+=1;
+    } else {
+      count+=1;
+      if (count % 2 != 0) {
+        result = A[i];
+        break;
+      }
+    }
+  }
+return result
+}
