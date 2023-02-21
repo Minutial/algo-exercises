d03dc782e1b2ab76b223c00c0062058e7ad17e4d
Hajnalka "Zerys" Bogdán <111108971+Minutial@users.noreply.github.com>
2023-02-21 15:42:57 +0100

    Typescript - Who likes it? - CW 6

---
+++ who_likes_it.ts
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/5266876b8f4bf2da9b000362
+
+export const likes = (a : string[]) : string => {
+    if(a.length > 3) {
+        return `${a[0]}, ${a[1]} and ${a.length -= 2} others like this`
+    } else if (a.length === 3) {
+        return `${a[0]}, ${a[1]} and ${a[2]} like this`
+    } else if (a.length === 2) {
+        return `${a[0]} and ${a[1]} like this`
+    } else if (a.length === 0) {
+        return "no one likes this"
+    } else {
+        return `${a[0]} likes this`
+    }
+};
